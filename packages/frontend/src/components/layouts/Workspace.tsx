66ae27da7f346043f79687032d5519ebb96d98a3
Scott Zonneveldt <scott.zonneveldt@howatsonco.com.au>
2025-03-03 12:25:33 +1100

    update workspace bg

--- packages/frontend/src/components/layouts/Workspace.tsx
+++ packages/frontend/src/components/layouts/Workspace.tsx
@@ -1,10 +1,11 @@
 import { Box } from "@radix-ui/themes";
 import { AssetRenderer } from "@/features/figma/components/AssetRenderer/AssetRenderer";
 import { useTemplateState } from "@/features/figma/context/TemplateContext";
+import styles from "./Workspace.module.scss";
 export function Workspace({ children }: { children?: React.ReactNode }) {
   const { overlayAssets, templateConfig, textInputs, customImage, customImageDefaults } = useTemplateState();
   return (
-    <Box p="6">
+    <Box p="6" className={styles.workspace}>
       <AssetRenderer
         customImage={customImage}
         selectedAssets={overlayAssets.map((asset) => ({
